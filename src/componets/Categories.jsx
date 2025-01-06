635090c12c234731c24cf00f3d1f597e85395fda
Ahasan Ullah <ahasanullah451@gmail.com>
2025-01-06 22:23:45 +0600

    category section modified

--- src/componets/Categories.jsx
+++ src/componets/Categories.jsx
@@ -1,23 +1,68 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper/modules';
+
+import slide1 from '../assets/football.jpg'
+import slide2 from '../assets//cricket.jpg'
+import slide3 from '../assets/badminton.jpg'
+import slide4 from '../assets/volleyball.jpg'
+import slide5 from '../assets/hockey.jpg'
+import slide6 from '../assets/rugby.jpg'
+
 const Categories = () => (
   <section id="categories" className="py-12 border-b">
     <div className="container mx-auto">
-      <h2 className="text-3xl font-bold text-center text-black mb-6">Sports Items Categories</h2>
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-6">
-        {[
-          "Football",
-          "Basketball",
-          "Tennis",
-          "Cricket",
-          "Hiking",
-          "Swimming",
-          "Cycling",
-          "Yoga",
-        ].map((category) => (
-          <div key={category} className="cursor-pointer border rounded-lg shadow-md text-center py-6 bg-gray-800 text-white hover:bg-gray-700 transition">
-            <h3 className="font-bold text-lg">{category}</h3>
-          </div>
-        ))}
-      </div>
+      <h2 className="text-3xl font-bold text-center text-text mb-6">
+        Sports Items Categories
+      </h2>
+      <Swiper
+        slidesPerView={4}
+        spaceBetween={30}
+        centeredSlides={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper mb-24"
+      >
+        <SwiperSlide>
+          <img className='w-full h-56' src={slide1} alt="" />
+          <h3 className="text-2xl uppercase text-center -mt-16 text-white">
+            Football
+          </h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className='w-full h-56' src={slide2} alt="" />
+          <h3 className="text-2xl uppercase text-center -mt-16 text-white">
+            Cricket
+          </h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className='w-full h-56' src={slide3} alt="" />
+          <h3 className="text-2xl uppercase text-center -mt-16 text-white">
+            Badminton
+          </h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className='w-full h-56' src={slide4} alt="" />
+          <h3 className="text-2xl uppercase text-center -mt-16 text-white">
+            Volleyball
+          </h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className='w-full h-56' src={slide5} alt="" />
+          <h3 className="text-2xl uppercase text-center -mt-16 text-white">
+            Hockey
+          </h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className='w-full h-56' src={slide6} alt="" />
+          <h3 className="text-2xl uppercase text-center -mt-16 text-white">
+            Rugby
+          </h3>
+        </SwiperSlide>
+      </Swiper>
     </div>
   </section>
 );
