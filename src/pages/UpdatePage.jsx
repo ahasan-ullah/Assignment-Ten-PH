df8f87b97649c0f6a5c4b92430655452dc4e2dd2
Ahasan Ullah <ahasanullah451@gmail.com>
2025-01-07 00:30:29 +0600

    modified

--- src/pages/UpdatePage.jsx
+++ src/pages/UpdatePage.jsx
@@ -255,7 +255,7 @@ const UpdatePage = () => {
           </div>
           <button
             type="submit"
-            className="w-full bg-blue-600 text-white py-3 rounded-lg hover:bg-blue-700 transition"
+            className="w-full bg-first text-white py-3 rounded-lg hover:bg-blue-700 transition"
           >
             Update
           </button>
